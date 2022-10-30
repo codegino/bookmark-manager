86e12281a882f2dbda969da2feb2912c965279d8
codegino <carlo.gino.catapang@devoteam.com>
2022-10-30 21:58:40 +0100

    feat(api): Added supabase client

--- src/pages/newtab/Newtab.tsx
+++ src/pages/newtab/Newtab.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "@assets/img/logo.svg";
 import "@src/styles/app.css";
 import "@pages/newtab/Newtab.css";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 const Newtab = () => {
+  const [todos, setTodos] = useState<BookMark[]>([]);
+
+  useEffect(() => {
+    supabase
+      .from("bookmarks")
+      .select("id,name,url")
+      .then((res) => {
+        const { data: bookmarks, error } = res;
+
+        setTodos(bookmarks as BookMark[]);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,6 +29,12 @@ const Newtab = () => {
         <p className="text-red-500">
           Edit <code>src/pages/newtab/Newtab.tsx</code> and save to reload.
         </p>
+        {todos.map((todo) => (
+          <div key={todo.id}>
+            <p>{todo.name}</p>
+            <p>{todo.url}</p>
+          </div>
+        ))}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -23,6 +47,12 @@ const Newtab = () => {
       </header>
     </div>
   );
+};
+
+type BookMark = {
+  id: number;
+  name: string;
+  url: string;
 };
 
 export default Newtab;
